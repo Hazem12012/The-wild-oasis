9eef22facfdd1a3b15f815ad65b7ad9fbca670bb
Hazem12012 <hazemmahmoud12012@gmail.com>
2026-06-27 08:27:06 +0300

    Enhance Sort filter

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -1,4 +1,4 @@
-import { useSearchParams } from "react-router-dom";
+import { StaticRouterProvider, useSearchParams } from "react-router-dom";
 import { useGetBookings } from "./useBookings";
 import { tableHeader } from "../../utils/bookingConstants";
 import Spinner from "../../ui/Spinner";
@@ -11,8 +11,14 @@ function BookingTable() {
   const { bookings, isLoading } = useGetBookings();
 
   const [searchParams] = useSearchParams();
-  const filterValue = searchParams.get("status") || "all";
 
+  const filterValue = searchParams.get("status") || "all";
+  const sortValue = searchParams.get("sortBy") || "startDate-asc";
+
+  if (isLoading) return <Spinner />;
+  if (!bookings?.length) return <Empty resourceName="bookings" />;
+
+  // sorting logic
   let filterBookings;
 
   if (filterValue === "all") {
@@ -33,8 +39,23 @@ function BookingTable() {
     filterBookings = bookings;
   }
 
-  if (isLoading) return <Spinner />;
-  if (!bookings?.length) return <Empty resourceName="bookings" />;
+  // sort logic
+
+  const [field, direction] = sortValue.split("-");
+  const modifier = direction === "desc" ? -1 : 1;
+
+  if (field) {
+    filterBookings.sort((a, b) => {
+      if (field === "startDate") {
+        return (
+          (new Date(a[field]).getTime() - new Date(b[field]).getTime()) *
+          modifier
+        );
+      }
+
+      return (a[field] - b[field]) * modifier;
+    });
+  }
 
   return (
     <Menus>
--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -4,13 +4,11 @@ import BookingTable from "../features/bookings/BookingTable";
 import BookingTableOperations from "../features/bookings/BookingTableOperations";
 
 function Bookings() {
-
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All bookings</Heading>
-
-<BookingTableOperations/>
+        <BookingTableOperations />
       </Row>
 
       <BookingTable />
